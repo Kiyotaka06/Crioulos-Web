090a8f7948aa9b679c62b6838f5dc224b6eb4eeb
zlept55 <rochinhas.pais@gmail.com>
2024-03-06 10:39:24 +0000

    added notion and origins view and imgs

---
+++ resources/views/history/notion.blade.php
@@ -0,0 +1,29 @@
+<x-guestLayout>
+    <div class="my-8">
+        <a class="mb-2 font-bold text-5xl ml-8">Conceito de Crioulo</a>
+        <div class="grid grid-cols-4 gap-8 m-10 content-center text-lg">
+            <div class="col-span-3 flex flex-col justify-center space-y-4">
+                <div class="text-justify space-y-1">
+                    <p class="">A noção do crioulo, atualmente, refere-se àquilo que provém de países onde houve escravatura negra, ou no dialecto que resulta da evolução de uma língua a partir do contacto dos povos autóctones com os colonizadores.</p>
+                    <p class="">Para a linguistíca, o crioulo é a lingua natural resultante da mistura da língua autóctune com uma outra língua, tornando-se em língua materna</p>
+                </div>
+            </div>
+            <div class="flex flex-col items-center space-y-2">
+                <img class="rounded-xl drop-shadow-md" src="/imgs/historia_img2.png" alt="">
+            </div>
+        </div>
+
+        <div class="grid grid-cols-4 gap-8 m-10 content-center text-lg">
+            <div class="col-span-3 flex flex-col justify-center space-y-4">
+                <div class="text-justify space-y-1">
+                    <p class="">Segundo o linguista espanhol Lazaro Carreter, os crioulos, relativamente ao idioma, são formados a partir de línguas da Europa como o espanhol, o português, o françês ou o inglês.</p>
+                    <p class="">Para a linguistíca, o crioulo é a lingua natural resultante da mistura da língua autóctune com uma outra língua, tornando-se em língua materna</p>
+                </div>
+            </div>
+
+            <div class="flex flex-col items-center space-y-2">
+                <img class="rounded-xl drop-shadow-md" src="/imgs/" alt="">
+            </div>
+        </div>
+    </div>
+</x-guestLayout>
--- resources/views/history/origins.blade.php
+++ resources/views/history/origins.blade.php
@@ -1,8 +1,16 @@
 <x-guestLayout>
-    <div class="flex flex-col w-10/12 justify-center my-4 mx-auto space-x-8">
-        <div class="text-lg">
-            <div class="justify-center m-4 text-justify">
-                As origens do Crioulo Guineense ..
+    <div class="my-8">
+        <div class="grid grid-cols-4 gap-8 m-10 content-center text-lg">
+            <div class="col-span-3 flex flex-col justify-center space-y-4">
+                <a class="mb-2 font-bold text-5xl">Origens do Crioulo em Portugal</a>
+                <div class="text-justify space-y-1">
+                    <p class="">De origem portuguesa, o termo <a class="font-bold">crioulo</a> surgiu na época colonial para fazer referência às pessoas que, embora sejam descendentes de europeus, nasceram em países originárias da colonização europeia.</p>
+                    <p class="">Também era chamado <a class="font-bold">crioulos</a> aos negros nacidos no território americano, nomeadamente no <a class="font-bold">Brasil</a>, diferenciando esses cidadãos americanos de raça negra daqueles que tinham chegado da África com o cargo de escravos.</p>
+                </div>
+            </div>
+
+            <div class="flex flex-col items-center space-y-2">
+                <img class="rounded-xl drop-shadow-md" src="/imgs/historia_img1.png" alt="">
             </div>
         </div>
     </div>
